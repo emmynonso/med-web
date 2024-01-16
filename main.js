c38abbf75e08c5244f6d40febe6f44a5b518e291
Emmanuel Chinonso <emmanuelchinonso1340@gmail.com>
2024-01-16 22:16:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelector(".nav-links");
+      function onToggleMenu(e) {
+        e.name = e.name === "menu" ? "close" : "menu";
+        navLinks.classList.toggle("left-0");
+      }
+
+
+
+      
